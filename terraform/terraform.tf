798b5ce5522d3c33f368db6f5986f92be14256cf
Davis Weimer <davisweimer@gmail.com>
2025-11-23 09:45:04 -0700

    Place providers in main dir
    For the sake of the lab

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,3 +1,29 @@
 terraform {
-  required_version = ">=1.0.0"
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.76"
+    }
+
+    http = {
+      source = "hashicorp/http"
+      version = "~> 2.2"
+    }
+
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.7"
+    }
+
+    local = {
+      source = "hashicorp/local"
+      version = "~> 2.6"
+    }
+
+    tls = {
+      source = "hashicorp/tls"
+      version = "~> 3.1"
+    }
+  }
 }
